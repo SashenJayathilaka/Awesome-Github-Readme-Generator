cfdf647c6c7574a74ee2d9f917a89140036da466
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2023-05-25 09:09:38 +0530

    🐛 Bug Fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -87,6 +87,9 @@ function Footer({}: Props) {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023 . All Rights Reserved.
           </span>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            Made ❤️ by Sashen Jayathilaka
+          </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a
               href="#"
